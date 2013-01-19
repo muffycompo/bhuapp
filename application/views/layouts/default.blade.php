b2b2dc11d951c0f7bbe65ee9a9971802afb6ca22
Mfawa Alfred Onen <muffycompoqm@gmail.com>
2013-01-20 00:30:23 +0100

    Added new BHU Favicon to HTML markup

--- application/views/layouts/default.blade.php
+++ application/views/layouts/default.blade.php
@@ -2,7 +2,11 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8" />
-	<title>Bingham University - v1.0.1 beta</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>Bingham University - v1.1.0 beta</title>
+    <meta name="description" content="Bingham University Applicant Registration Portal">
+    <meta name="viewport" content="width=device-width">
+    <link rel="shortcut icon" href="{{ URL::to('favicon.ico') }}" type="image/x-icon" />
 	{{ HTML::style('/css/style.css') }}
 	<!--[if lt IE 7 ]>	<html class="ie6"> <![endif]-->
 	<!--[if lt IE 7 ]>
@@ -13,7 +17,9 @@
 	<![endif]-->
 </head>
 <body>
-
+	<!--[if lt IE 7]>
+		<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+	<![endif]-->
 	<div id="header">
 		
 		<div id="banner" class="container clearfix">
